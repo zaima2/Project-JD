90b6bbbb2bb48e85ac6e8f449a46c0f72149eb95
zaima? <1206691662@outlook.com>
2023-03-23 07:26:41 +0800

    fix: layout of each item

--- client/src/components/Goods.vue
+++ client/src/components/Goods.vue
@@ -1,120 +1,141 @@
 <template>
-    <div class="goods-container">
-        <div class="thumb" v-if="props.data.thumbs.length >= 1">
-            <img :src="server.server + props.data.thumbs[0]" alt="">
-        </div>
-        <div class="goods-info">
-            <div class="price">￥{{ props.data.price }}</div>
-            <div class="goods-name">
-                {{ props.data.name }}
-            </div>
-            <div class="comment">
-                <span>{{ 35320 }} </span>条评价
-            </div>
-            <div class="store-name">
-                <div class="name">IU发撒来看房价开始阶段了空间啊立刻就付了款解放军佛挖树机发</div>
-                <div class="icon">
-                    <Icon :type="StyleType.chat" />
-                </div>
-            </div>
-            <div class="tags">
-                <span class="tag-item" v-for="item in props.data.tags">{{ item }}</span>
-            </div>
-        </div>
-
+  <div class="goods-container">
+    <div class="thumb" v-if="props.data.thumbs.length >= 1">
+      <img :src="server.server + props.data.thumbs[0]" alt="" />
     </div>
+    <div class="goods-info">
+      <div class="price">￥{{ props.data.price }}</div>
+      <div class="goods-name hover-base">
+        {{ props.data.name }}
+      </div>
+      <div class="comment">
+        <span>{{ 35320 }} </span>条评价
+      </div>
+      <div class="store-name">
+        <div class="name">
+          IU发撒来看房价开始阶段了空间啊立刻就付了款解放军佛挖树机发
+        </div>
+        <div class="icon">
+          <Icon :type="StyleType.chat" />
+        </div>
+      </div>
+      <div class="tags">
+        <span class="tag-item" v-for="item in props.data.tags">{{ item }}</span>
+      </div>
+      <div class="footer">
+        <div class="like">
+          <Icon :type="StyleType.chat" />
+          <span>关注</span>
+        </div>
+        <div class="cart hover-base">
+          <Icon :type="StyleType.cartEmpty" :size="30" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 
 <script lang="ts" setup>
 import { Goods } from "../types/Goods";
 import server from "../configure";
 import Icon from "./Icon.vue";
-import { StyleType } from "../types/enum"
+import { StyleType } from "../types/enum";
 interface PropTypes {
-    data: Goods
+  data: Goods;
 }
 
 const props = defineProps<PropTypes>();
-
 </script>
-
 
 <style scoped lang="less">
 .goods-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .thumb {
     width: 100%;
-    height: 100%;
+    height: 50%;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .goods-info {
+    flex: 1;
+    width: 100%;
     display: flex;
     flex-direction: column;
-
-    .thumb {
-        width: 100%;
-        height: 50%;
-
-        img {
-            width: 100%;
-            height: 100%;
-        }
+    .price {
+      width: 100%;
+      height: 50px;
+      font-size: 30px;
+      color: @baseColor;
+      font-weight: bold;
     }
 
-    .goods-info {
-        flex: 1;
-        width: 100%;
-
-        .price {
-            width: 100%;
-            height: 50px;
-            font-size: 30px;
-            color: @baseColor;
-            font-weight: bold;
-        }
-
-        .goods-name {
-            width: 100%;
-            height: 35px;
-            -webkit-line-clamp: 2;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            line-height: calc(35px / 2)
-        }
-
-        .comment {
-            width: 100%;
-            height: 30px;
-            line-height: 30px;
-        }
-
-        .store-name {
-            width: 100%;
-            height: 30px;
-            display: flex;
-            align-items: center;
-
-            .name {
-                line-height: 30px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                flex: 1 1 auto;
-            }
-
-            .icon {
-                width: 60px;
-            }
-
-            .tags {
-                width: 100%;
-                height: 30px;
-                display: flex;
-
-                .tag-item {
-                    // margin: 0 5px;
-                    // box-sizing: border-box;
-                    // padding: 5px;
-                    // background-color: @baseColor;
-                }
-            }
-        }
+    .goods-name {
+      width: 100%;
+      height: 35px;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      line-height: calc(35px / 2);
     }
+
+    .comment {
+      width: 100%;
+      height: 30px;
+      line-height: 30px;
+    }
+
+    .store-name {
+      width: 100%;
+      height: 30px;
+      display: flex;
+      align-items: center;
+
+      .name {
+        line-height: 30px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        flex: 1 1 auto;
+      }
+
+      .icon {
+        width: 60px;
+      }
+    }
+
+    .tags {
+      width: 100%;
+      height: 30px;
+      display: flex;
+
+      .tag-item {
+        margin: 0 5px;
+        box-sizing: border-box;
+        padding: 3px 5px;
+        border-radius: 5px;
+        color: #fff;
+        height: fit-content;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: @baseColor;
+      }
+    }
+    .footer {
+      flex: 1;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
 }
 </style>
--- client/src/views/Search/index.vue
+++ client/src/views/Search/index.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="search-container">
-        <div class="banner">
-            <Banner />
-        </div>
-        <div class="content">
-            <div class="recommadation">推荐栏</div>
-            <div class="main">
-                <div class="item" v-for="item in state.data">
-                    <GoodComp :data="item" />
-                </div>
-            </div>
-        </div>
+  <div class="search-container">
+    <div class="banner">
+      <Banner />
     </div>
+    <div class="content">
+      <div class="recommadation">推荐栏</div>
+      <div class="main">
+        <div class="item" v-for="item in state.data">
+          <GoodComp :data="item" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 
 <script lang="ts" setup>
 import { watchEffect, reactive } from "vue";
@@ -25,63 +24,68 @@ import Banner from "../Home/components/Banner.vue";
 import GoodComp from "../../components/Goods.vue";
 const route = useRoute();
 const state = reactive({
-    total: 0,
-    data: [] as Goods[]
+  total: 0,
+  data: [] as Goods[],
 });
 
 watchEffect(async () => {
-    if (route.query.page && route.query.limit) {
-        const resp = await searchGoods(route.query.keyword as string, +route.query.page, +route.query.limit) as unknown as ResponseWithCount<Goods>;
-        state.total = resp.count;
-        state.data = resp.rows;
-        console.log("resp:", resp);
-    }
-})
-
-
+  if (route.query.page && route.query.limit) {
+    const resp = (await searchGoods(
+      route.query.keyword as string,
+      +route.query.page,
+      +route.query.limit
+    )) as unknown as ResponseWithCount<Goods>;
+    state.total = resp.count;
+    state.data = resp.rows;
+    console.log("resp:", resp);
+  }
+});
 </script>
-
 
 <style scoped lang="less">
 .search-container {
-    width: 100%;
-    flex: 1;
-    // background-color: #fff;
-    background-color: red;
-    overflow: scroll;
-    overflow-x: hidden;
+  width: 100%;
+  flex: 1;
+  background-color: #fff;
+  // background-color: red;
+  overflow: scroll;
+  overflow-x: hidden;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  .content {
+    width: 1400px;
+    height: 100%;
+    // background-color: blue;
+    margin-right: 200px;
+    margin-top: 20px;
     display: flex;
-    align-items: center;
-    flex-direction: column;
 
-    .content {
-        width: 1400px;
-        height: 100%;
-        background-color: blue;
-        margin-right: 200px;
-        margin-top: 20px;
-        display: flex;
-
-        .recommadation {
-            width: 200px;
-            height: 100%;
-        }
-
-        .main {
-            flex: 1 0 auto;
-            background-color: aqua;
-            display: flex;
-            flex-wrap: wrap;
-
-            .item {
-                width: 260px;
-                height: 450px;
-                box-sizing: border-box;
-                border: 1px solid #ccc;
-                margin: 10px;
-                padding: 10px;
-            }
-        }
+    .recommadation {
+      width: 200px;
+      height: 100%;
     }
+
+    .main {
+      flex: 1 0 auto;
+      // background-color: aqua;
+      display: flex;
+      flex-wrap: wrap;
+
+      .item {
+        width: 260px;
+        height: 450px;
+        box-sizing: border-box;
+        // border: 1px solid #ccc;
+        margin: 10px;
+        padding: 10px;
+        &:hover {
+          box-shadow: 0px 0px 1px #000;
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 </style>
